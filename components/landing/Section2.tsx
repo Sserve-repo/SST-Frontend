46b0b5e4fdf067f122197d915e37759d825d28eb
Timmy-Edibo <timothyedibo@gmail.com>
2024-09-27 04:48:03 +0100

    landing page

---
+++ components/landing/Section2.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import verifiedIcon from "../../assets/images/verifiedIcon.svg";
+import Property from "../../assets/images/property.png";
+
+const Section2 = () => {
+  const Items = [
+    "Quality Guaranteed",
+    "Direct Access to Trusted Experts",
+    "Seamless & Secure Transactions",
+    "Fair Pricing & Transparent Deals",
+    "Tailored Solutions for Every Need",
+    "Ongoing Support & Assistance",
+  ];
+  return (
+    <div className="grid grid-cols-2 bg-white  mt-20 min-h-screen px-[6rem] font-primaryFont">
+      <div className="flex flex-col justify-start items-start  text-[#502266] gap-y-8 mt-auto">
+        <h1 className="text-[40px] ">Get More with SphereServe</h1>
+        <div>
+          {Items.map((item, index) => {
+            return (
+              <div className="flex gap-x-4" key={index}>
+                <Image src={verifiedIcon} alt="verifiedIcon"></Image>
+                <p className="text-[25px] ">{item}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        <p className="py-2 my-8 text-white bg-[#502266] rounded w-[14rem] text-center hover:cursor-pointer">
+          Get Started
+        </p>
+      </div>
+      <div className=" mt-auto">
+        <Image className="relative" src={Property} alt="property" />
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
---
+++ components/landing/Section4.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import verifiedIcon from "../../assets/images/verifiedIcon.svg";
+import Tailor from "../../assets/images/tailor.png";
+
+const Section4 = () => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-white pt-10  min-h-screen px-[6rem] font-primaryFont">
+      <h1 className="text-[40px] text-[#502266] font-semibold">
+        Recommended Vendors
+      </h1>
+      <div className="grid grid-cols-4 gap-4 w-full">
+        {[1, 2, 3, 4, 5, 6,7,8].map((item, index) => {
+          return (
+            <div className="border h-[20rem] w-[18rem]  rounded-lg" key={index}>
+              <div className="grid grid-rows-2 h-[50%] overflow-hidden">
+                <Image className="rounded-lg" src={Tailor} alt="tailor" />
+              </div>
+              <div className="flex flex-col pl-6 font-semibold pt-2 bg-[#240F2E] text-white h-[50%] rounded-b-lg">
+                <p className="text-[18px]">Building Solutions Co.</p>
+                <p className="text-[13px] text-[#FFCA95] rounded-lg p-1">
+                  Reliable Supplier for Builders
+                </p>
+                <p>⭐ (3.5/5.0)</p>
+                <div className="p-2 bg-[#FF7F00] rounded-lg text-white w-[6rem] hover:cursor-pointer">
+                  Explore
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
