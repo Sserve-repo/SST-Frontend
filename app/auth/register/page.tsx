32e4e9c7a5d8c8e9bb7dd07fbc92c570bfc644ac
Onaivio <onaivio220@gmail.com>
2024-09-30 22:10:54 +0100

    Add LoginPage component with role selection

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Role from "./_components/Role";
+
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full min-h-screen flex-col">
+        <Role />
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
