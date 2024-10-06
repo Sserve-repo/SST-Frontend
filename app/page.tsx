ede312e2bf2c5691f5ef88372b5188ef531f4c8c
Timmy-Edibo <timothyedibo@gmail.com>
2024-10-06 15:39:23 +0100

    made minor fixes 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/landing/Hero";
-import Section1 from "@/components/landing/Section1";
+// import Section1 from "@/components/landing/Section1";
 import Section2 from "@/components/landing/Section2";
 import Section3 from "@/components/landing/Section3";
 import Section4 from "@/components/landing/Section4";
@@ -7,7 +7,7 @@ import Section5 from "@/components/landing/Section5";
 import Section6 from "@/components/landing/Section6";
 import Section7 from "@/components/landing/Section8";
 import Section9 from "@/components/landing/Section9";
-import Section8 from "@/components/landing/Section9";
+// import Section8 from "@/components/landing/Section9";
 
 export default function Home() {
   return (
