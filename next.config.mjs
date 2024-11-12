faf9953c4ed7898a3c13cabb12f4fed6e66236c9
Onaivio <onaivio220@gmail.com>
2024-11-12 07:35:59 +0100

    add remote image patterns for Pexels and Unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,22 @@ import {
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 const nextConfigFunction = async (phase) => {
