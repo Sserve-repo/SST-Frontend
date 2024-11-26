efa498f40246f0c9262c2129f500923342468f83
Onaivio <onaivio220@gmail.com>
2024-11-26 11:50:31 +0100

    add service actions for fetching regions, services menu, and services by category

---
+++ actions/service.ts
@@ -0,0 +1,43 @@
+import { baseUrl } from "../config/constant";
+
+export const getRegions = async () => {
+  try {
+    const response = await fetch(`${baseUrl}/products/getProductRegion`);
+    return response;
+  } catch (error: any) {
+    console.log("failed to fetch regions", error);
+  }
+};
+
+export const getServiceByRegions = async (id: number) => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/products/getCategoryItemsByRegion/${id}`
+    );
+    return response;
+  } catch (error: any) {
+    console.log("failed to fetch product items regions", error);
+  }
+};
+export const getServicesMenu = async () => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/general/services/getCategoryMenu`,
+      { next: { revalidate: 86400 } }
+    );
+    return response;
+  } catch (error: any) {
+    console.log("failed to fetch service menu", error);
+  }
+};
+
+export const getServiceByCategory = async (catId: number) => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/general/services/geServiceByCategorySub?limit=20&page=1&service_category=${catId}`
+    );
+    return response;
+  } catch (error: any) {
+    console.log("failed to fetch service menu", error);
+  }
+};
