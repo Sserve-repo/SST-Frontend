6e751fa609f10cf7542812e6dce2be0757790d3a
Onaivio <onaivio220@gmail.com>
2024-12-10 23:39:18 +0100

    add Home page component with multiple sections

---
+++ app/(site)/page.tsx
@@ -0,0 +1,25 @@
+import Hero from "@/components/landing/Hero";
+import Section2 from "@/components/landing/Section2";
+import Section3 from "@/components/landing/Section3";
+import Section4 from "@/components/landing/Section4";
+import Section5 from "@/components/landing/Section5";
+import Section6 from "@/components/landing/Section6";
+import Section7 from "@/components/landing/Section7";
+import Section8 from "@/components/landing/Section8";
+import Section9 from "@/components/landing/Section9";
+
+export default function Home() {
+  return (
+    <div className=" w-full">
+      <Hero />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Section8 />
+      <Section9/>
+    </div>
+  );
+}
