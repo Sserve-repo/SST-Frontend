3fffe908a30e0047e140958c94c8d2e88c8f83cc
Onaivio <onaivio220@gmail.com>
2024-12-11 06:20:00 +0100

    add ContactUs component for the blog page

---
+++ app/(site)/about/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div className="relative grid grid-cols-1 bg-white min-h-screen w-full">
+      <div>dbddbbbbbbbbbbbbbbbbbbb</div>
+    </div>
+  );
+};
+
+export default ContactUs;
