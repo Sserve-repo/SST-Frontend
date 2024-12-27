6caaabdad212cde96cdc6e838c28a239c79e325c
Timmy-Edibo <timothyedibo@gmail.com>
2024-12-27 05:46:13 +0100

    fixed profile page responsiveness2

--- app/auth/register/_components/RoleWithRedirect.tsx
+++ app/auth/register/_components/RoleWithRedirect.tsx
@@ -23,20 +23,22 @@ export default function Role() {
   return (
     <div className="container mx-auto p-4 justify-center w-full items-center flex flex-col">
       <div className="w-full max-w-md">
-        <div className="flex justify-center items-center w-full">
+        <div className="flex justify-center items-center w-full mb-8">
           <div className="text-left">
-            <h1 className="text-[55px] font-bold text-primary">Join Us!</h1>
-            <p className="text-[25px] font-normal text-[#502266]">
+            <h1 className="text-3xl sm:text-[55px] font-bold text-primary">
+              Join Us!
+            </h1>
+            <p className="text-xl sm:text-[25px] font-normal text-[#502266]">
               To begin this journey, tell us what type of account you&apos;d be
               opening.
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-3 w-full my-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-3 w-full my-8">
           {(["artisan", "vendor", "buyer"] as const).map((role) => (
             <Link href={`/auth/register?role=${role}`} key={role}>
               <Card className="cursor-pointer hover:shadow-lg transition-all bg-white hover:bg-primary group">
-                <CardHeader className="flex p-5 items-center flex-row gap-4">
+                <CardHeader className="flex p-5 items-center flex-col sm:flex-row gap-4">
                   <div className="p-3 rounded-full border-2 border-primary group-hover:border-white transition-colors">
                     {React.createElement(roleIcons[role], {
                       className: "text-2xl text-primary group-hover:text-white",
@@ -56,7 +58,7 @@ export default function Role() {
             </Link>
           ))}
         </div>
-        <div className="flex items-center w-full text-[#b9b9b9] font-normal text-lg mt-4">
+        <div className="flex items-center justify-center sm:justify-start w-full text-[#b9b9b9] font-normal text-lg mt-4">
           Already have an account?{" "}
           <a
             href="/auth/login"
