7d3929ea6419916675261bc961430f10c46cfefc
Timmy-Edibo <timothyedibo@gmail.com>
2024-12-28 09:32:30 +0100

    made minor fixes2

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import LoginForm from "./_components/LoginForm";
 import { Splash } from "../_components/splash";
 
 const RegisterPage = () => {
   return (
     <>
-      <div className="flex items-center justify-center w-full min-h-screen ">
-        <div className="hidden lg:col-span-5 lg:block h-full overflow-hidden">
-          <Splash />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="flex items-center justify-center w-full min-h-screen ">
+          <div className="hidden lg:col-span-5 lg:block h-full overflow-hidden">
+            <Splash />
+          </div>
+          <div className="container mx-auto p-4 justify-center w-full items-center flex flex-col">
+            <LoginForm />
+          </div>
         </div>
-        <div className="container mx-auto p-4 justify-center w-full items-center flex flex-col">
-          <LoginForm />
-        </div>
-      </div>
+      </Suspense>
     </>
   );
 };
