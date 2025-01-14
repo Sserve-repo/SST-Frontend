0fa24e27632d2281eaef47cb2f862f4a8e9d6ee4
onaivio <onaivio220@gmail.com>
2025-01-14 17:05:56 +0100

    add success page with authentication layout and success message component

---
+++ app/auth/success/page.tsx
@@ -0,0 +1,12 @@
+import { AuthLayout } from "../_components/auth-layout";
+import { SuccessMessage } from "../_components/success-message";
+
+
+export default function SuccessPage() {
+  return (
+    <AuthLayout>
+      <SuccessMessage />
+    </AuthLayout>
+  )
+}
+
