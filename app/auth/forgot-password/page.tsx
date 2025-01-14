0bda9beb922c0a524fd4826151f253948a3f37cc
onaivio <onaivio220@gmail.com>
2025-01-14 17:06:05 +0100

    add forgot password page with authentication layout and form

---
+++ app/auth/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import { AuthLayout } from "../_components/auth-layout";
+import { ForgotPasswordForm } from "../_components/forgot-password-form";
+
+
+export default function ForgotPasswordPage() {
+  return (
+    <AuthLayout>
+      <ForgotPasswordForm />
+    </AuthLayout>
+  )
+}
+
