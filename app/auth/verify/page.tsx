97baa81fd6bfab863ea7875cb8e3e9ba54211ef7
onaivio <onaivio220@gmail.com>
2025-01-14 17:05:50 +0100

    add verification page with authentication layout and form

---
+++ app/auth/verify/page.tsx
@@ -0,0 +1,12 @@
+import { AuthLayout } from "../_components/auth-layout";
+import { VerificationForm } from "../_components/verification-form";
+
+
+export default function VerifyPage() {
+  return (
+    <AuthLayout>
+      <VerificationForm />
+    </AuthLayout>
+  )
+}
+
