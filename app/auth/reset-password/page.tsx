4e84952be6aea4666d1031163a3624c2245945c1
onaivio <onaivio220@gmail.com>
2025-01-14 17:06:16 +0100

    add reset password page with authentication layout and form

---
+++ app/auth/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import { AuthLayout } from "../_components/auth-layout";
+import { ResetPasswordForm } from "../_components/eset-password-form";
+
+
+export default function ResetPasswordPage() {
+  return (
+    <AuthLayout>
+      <ResetPasswordForm />
+    </AuthLayout>
+  );
+}
