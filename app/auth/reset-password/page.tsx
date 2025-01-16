febb652efe7d3e96c186f8790496ca29e5d59eb7
onaivio <onaivio220@gmail.com>
2025-01-16 21:36:37 +0100

    fix import path for ResetPasswordForm component in reset password page

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,5 +1,5 @@
 import { AuthLayout } from "../_components/auth-layout";
-import { ResetPasswordForm } from "../_components/eset-password-form";
+import { ResetPasswordForm } from "../_components/reset-password-form";
 
 
 export default function ResetPasswordPage() {
