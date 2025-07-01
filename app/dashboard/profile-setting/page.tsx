e455e67c2e868f70d94ca8a2615e4e1ea0274102
onaivio <onaivio220@gmail.com>
2025-07-01 12:53:10 +0100

    refactor: update import path for UnifiedProfileSettings component

--- app/dashboard/profile-setting/page.tsx
+++ app/dashboard/profile-setting/page.tsx
@@ -1,6 +1,7 @@
+import UnifiedProfileSettings from "@/components/settings/unified-profile-settings";
 import { Metadata } from "next";
 // import ProfileSettingPage from "./_components/ProfileSetting";
-import UnifiedProfileSettings from "@/components/profile/unified-profile-settings";
+// import UnifiedProfileSettings from "@/components/profile/unified-profile-settings";
 
 export const metadata: Metadata = {
   title: "Setting | Profile",
